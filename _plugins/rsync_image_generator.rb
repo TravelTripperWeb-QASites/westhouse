94941916e427bd074d8afd9ccda018dac9968794
Alexander Grebennik <aleks.grebennik@gmail.com>
2016-03-23 20:32:00 +0300

    add rsync plugin

--- _plugins/rsync_image_generator.rb
+++ _plugins/rsync_image_generator.rb
@@ -1,9 +1,12 @@
 module Jekyll
   class RsyncImageGenerator < Generator
     def generate(site)
-      return
-      system('mkdir -p _site');
-      system('rsync --archive --delete _images/ _site/images/');
+      system("mkdir -p #{site.config['destination']}");
+
+      site.config['rsync_copy'].each do |path|
+        new_path = path[1..-1]
+        system("rsync --archive --delete #{path}/ #{site.config['destination']}/#{new_path}/")
+      end
     end
   end
 end
