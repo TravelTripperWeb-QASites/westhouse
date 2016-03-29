6c12992d238a58ab9eef68ec0991ee04cae39cc1
Alex Mekelburg <alex@mekware.com>
2016-03-29 15:29:24 -0400

    update to latest

--- _plugins/rsync_image_generator.rb
+++ _plugins/rsync_image_generator.rb
@@ -3,9 +3,11 @@ module Jekyll
     def generate(site)
       system("mkdir -p #{site.config['destination']}");
 
+      root = site.config['source']
       site.config['rsync_copy'].each do |path|
+        full_path = File.join(root,path)
         new_path = path[1..-1]
-        system("rsync --archive --delete #{path}/ #{site.config['destination']}/#{new_path}/")
+        system("rsync --archive --delete #{full_path}/ #{site.config['destination']}/#{new_path}/")
       end
     end
   end
